bf3f7912c645124acfe6554649365fc3e3d6c9fc
olivers-x <oliver.surina@external.erstegroup.com>
2024-12-13 12:21:14 +0100

    day13 - using Cramer rule

--- 2024/13/part2.mjs
+++ 2024/13/part2.mjs
@@ -5,6 +5,10 @@ function isWithinEpsilon(number, epsilon = 0.001) {
   return Math.abs(number - Math.round(number)) <= epsilon;
 }
 
+function isWhole(a) {
+  return a == Math.round(a);
+}
+
 const regex = /\d+/g;
 
 const math = Mathjs.create(Mathjs.all, { number: "bigint" });
@@ -17,21 +21,32 @@ async function part2(input) {
       const condition = line.split("\n");
       const [a1, a2] = condition[0].match(regex);
       const [b1, b2] = condition[1].match(regex);
-      let [aR, bR] = condition[2].match(regex);
+      let [c1, c2] = condition[2].match(regex);
 
-      aR = 10000000000000n + BigInt(aR);
-      bR = 10000000000000n + BigInt(bR);
+      c1 = 10000000000000n + BigInt(c1);
+      c2 = 10000000000000n + BigInt(c2);
 
-      const [[a], [b]] = math.lusolve(
-        [
-          [a1, b1],
-          [a2, b2],
-        ],
-        [aR, bR]
-      );
+      // https://en.wikipedia.org/wiki/Cramer%27s_rule
+      const cm = [
+        [a1, b1],
+        [a2, b2],
+      ];
 
-      if (a > 0 && b > 0 && isWithinEpsilon(a) && isWithinEpsilon(b)) {
-        return BigInt(Math.round(a)) * 3n + BigInt(Math.round(b));
+      const xm = [
+        [c1, b1],
+        [c2, b2],
+      ];
+
+      const ym = [
+        [a1, c1],
+        [a2, c2],
+      ];
+
+      const x = math.chain(math.det(xm)).divide(math.det(cm)).done();
+      const y = math.chain(math.det(ym)).divide(math.det(cm)).done();
+
+      if (isWhole(x) && isWhole(y)) {
+        return BigInt(x) * 3n + BigInt(y);
       }
 
       return 0n;
@@ -41,7 +56,9 @@ async function part2(input) {
   return price;
 }
 
-console.log(await part2(read()));
+const r = await part2(read());
+console.assert(r === 87550094242995n, { r });
+console.log(r);
 
 // 2312633642825590265 // too high
 // 87550094242995 // correct
