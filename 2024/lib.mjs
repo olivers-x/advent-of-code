9433df02ec993a0d4e8d3e732c785f3cc4a08585
olivers-x <oliver.surina@external.erstegroup.com>
2024-12-25 22:25:06 +0100

    clean up lib.mjs

--- 2024/lib.mjs
+++ 2024/lib.mjs
@@ -15,29 +15,6 @@ export function read() {
   return readFileSync(fileName, { encoding: "utf-8", flag: "r" }).trimEnd();
 }
 
-// Array.prototype.reshape = function (rows, cols) {
-//   var copy = this.slice(0); // Copy all elements.
-//   const l = this.length;
-//   this.length = 0; // Clear out existing array.
-
-//   cols ??= l / rows;
-
-//   // if (!cols) cols = rows;
-
-//   for (var r = 0; r < rows; r++) {
-//     var row = [];
-//     for (var c = 0; c < cols; c++) {
-//       var i = r * cols + c;
-//       if (i < copy.length) {
-//         row.push(copy[i]);
-//       }
-//     }
-//     this.push(row);
-//   }
-
-//   return this;
-// };
-
 Array.prototype.group = function (callback) {
   return this.reduce((acc = {}, ...args) => {
     const key = callback(...args);
