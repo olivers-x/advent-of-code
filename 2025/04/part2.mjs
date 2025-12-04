881a3032a9376622f5d5bcc876c956528d51e09d
olivers-x <oliver.surina@external.erstegroup.com>
2025-12-04 09:26:57 +0100

    2025 - Day 4

---
+++ 2025/04/part2.mjs
@@ -0,0 +1,47 @@
+import { read, sum } from '../lib.mjs';
+
+function part2(input) {
+  const grid = input.split('\n').map((line) => line.split(''));
+  const grid2 = input.split('\n').map((line) => line.split(''));
+
+  const rows = grid.length;
+  const columns = grid[0].length;
+
+  let count = 0;
+
+  const check = (x, y) => {
+    if (x < 0) return false;
+    if (y < 0) return false;
+    if (x === rows) return false;
+    if (y === columns) return false;
+    return grid[x][y] === '@';
+  };
+
+  for (let l = 0; l <= 30000; l++) {
+    for (let i = 0; i < rows; i++)
+      for (let j = 0; j < columns; j++) {
+        if (grid[i][j] !== '@') continue;
+        const adj = [
+          check(i - 1, j - 1),
+          check(i, j - 1),
+          check(i + 1, j - 1),
+          check(i - 1, j),
+          check(i + 1, j),
+          check(i - 1, j + 1),
+          check(i, j + 1),
+          check(i + 1, j + 1),
+        ]
+          .map(Number)
+          .reduce(sum);
+
+        if (adj < 4) {
+          count++;
+          grid[i][j] = 'x';
+        }
+      }
+  }
+
+  console.log(count);
+}
+
+part2(read());
